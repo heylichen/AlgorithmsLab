6f0fd977836bc3e28d6f80e97f497c89a57d6ef1
heylichen <heylichen@qq.com>
2018-04-17 22:10:34 +0800

    add RabinKarpSearcher for substring search

---
+++ src/main/java/algorithms/sedgewick/strings/substring/MyTest.java
@@ -0,0 +1,11 @@
+package algorithms.sedgewick.strings.substring;
+
+/**
+ * Created by Chen Li on 2018/4/17.
+ */
+public class MyTest {
+
+  public static void main(String[] args) {
+    System.out.println(Long.MAX_VALUE);
+  }
+}
