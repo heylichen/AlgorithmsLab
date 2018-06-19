774be50ffa845cabe9a01215b3fe61d60d67c4ce
heylichen <heylichen@qq.com>
2018-06-19 22:41:44 +0800

    add HuffmanCompression

--- src/test/java/algorithms/strings/compression/CharNodeTest.java
+++ src/test/java/algorithms/strings/compression/CharNodeTest.java
@@ -1,10 +1,13 @@
 package algorithms.strings.compression;
 
-import java.util.HashMap;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
-import algorithms.sorting.PriorityQueue;
-import algorithms.sorting.heap.PriorityQueueFactory;
+import edu.princeton.cs.algs4.BinaryIn;
+import edu.princeton.cs.algs4.BinaryOut;
 import org.junit.Test;
 
 /**
@@ -12,36 +15,69 @@ import org.junit.Test;
  */
 public class CharNodeTest {
 
+  private HuffmanCompression huffmanCompression = new HuffmanCompression();
+
   @Test
-  public void prefixFreeTrieConstructionTest() {
+  public void prefixFreeTrieConstructionTest() throws Exception {
     String text = "it was the best of times it was the worst of times\n";
-    Map<Character, Integer> freqMap = new HashMap<>();
-    for (int i = 0; i < text.length(); i++) {
-      Character ch = text.charAt(i);
-      Integer count = freqMap.computeIfAbsent(ch, k -> 0);
-      freqMap.put(ch, count + 1);
-    }
+    CharNode root = huffmanCompression.buildTrie(new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8)));
+    System.out.println(huffmanCompression.compressedBits(root));
+  }
 
-    PriorityQueue<CharNode> minPq = new PriorityQueueFactory().minPQ(freqMap.size());
-    for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
-      minPq.insert(new CharNode(null, null, entry.getKey(), entry.getValue(), true));
-    }
-
-    CharNode root = null;
-    while (!minPq.isEmpty()) {
-      CharNode charNode1 = minPq.pop();
-      CharNode charNode2 = null;
-      if (minPq.isEmpty()) {
-        root = charNode1;
-        break;
-      } else {
-        charNode2 = minPq.pop();
-      }
-
-      int freq = charNode1.getFrequency() + charNode2.getFrequency();
-      CharNode merged = new CharNode(charNode1, charNode2, '\0', freq, false);
-      minPq.insert(merged);
-    }
+  @Test
+  public void name() throws Exception {
+    String text = "AAAEEFG";
+    InputStream in = new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8));
+    CharNode root = huffmanCompression.buildTrie(in);
     System.out.println("ok");
   }
+
+  @Test
+  public void builMaptest() throws Exception {
+    String text = "AAAEEFG";
+    InputStream in = new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8));
+    CharNode root = huffmanCompression.buildTrie(in);
+    Map<Character, String> map = huffmanCompression.buildCharacterMap(root);
+    for (Map.Entry<Character, String> entry : map.entrySet()) {
+      System.out.println(entry.getKey() + " : " + entry.getValue());
+    }
+  }
+
+  @Test
+  public void compressAndExpandTest() throws Exception {
+    String text = "it was the best of times it was the worst of times\n";
+    InputStream in1 = new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8));
+    CharNode trie = huffmanCompression.buildTrie(in1);
+
+    //can not read the same InputStream twice, so create another instance
+    InputStream in = new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8));
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    huffmanCompression.compress(in, bos, trie);
+
+    ByteArrayInputStream byteIn = new ByteArrayInputStream(bos.toByteArray());
+    ByteArrayOutputStream expanded = new ByteArrayOutputStream();
+    huffmanCompression.expand(byteIn, expanded);
+
+    System.out.println(expanded.toString(StandardCharsets.UTF_8.name()));
+  }
+
+  @Test
+  public void codeTest() throws Exception {
+    String text = "AAAEEFG";
+    InputStream in = new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8));
+    CharNode root = huffmanCompression.buildTrie(in);
+    //
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    BinaryOut bo = new BinaryOut(bos);
+    huffmanCompression.writeTrie(root, bo);
+    bo.close();
+    //
+    ByteArrayInputStream byteIn = new ByteArrayInputStream(bos.toByteArray());
+    BinaryIn bin = new BinaryIn(byteIn);
+    CharNode node = huffmanCompression.readTrie(bin);
+    //
+    System.out.println("ok");
+  }
+
+
 }
