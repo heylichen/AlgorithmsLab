87176b4d8c3f79ef414eb7d7aa1e68f08a254408
heylichen <heylichen@qq.com>
2018-06-28 22:53:48 +0800

    add divide and conquer package, add Maximum subarray problem

--- src/main/java/algorithms/sorting/quick/partition/Partitioner.java
+++ src/main/java/algorithms/sorting/quick/partition/Partitioner.java
@@ -5,7 +5,14 @@ import org.apache.commons.lang3.tuple.Pair;
 /**
  * Created by Chen Li on 2018/2/1.
  */
-public interface Partitioner<T extends Comparable<T>> {
+public interface Partitioner<T> {
 
+  /**
+   * given an array, start index low, end index high( end index include),
+   * partition the array into two or three parts,
+   * return left middle index and right middle index
+   * if right middle index = left middle +1, then partition the array into 2 parts,
+   * if right middle index > left middle +1, then partition the array into 3 parts
+   */
   Pair<Integer, Integer> partition(T[] arr, int low, int high);
 }
