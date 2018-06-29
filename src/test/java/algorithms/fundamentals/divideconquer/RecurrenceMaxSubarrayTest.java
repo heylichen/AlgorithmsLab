382f2704751fcc3835b0f9820fa4422ff4208609
heylichen <heylichen@qq.com>
2018-06-29 23:21:10 +0800

    add Kadane's algorithm for maximum subarray problem

--- src/test/java/algorithms/fundamentals/divideconquer/RecurrenceMaxSubarrayTest.java
+++ src/test/java/algorithms/fundamentals/divideconquer/RecurrenceMaxSubarrayTest.java
@@ -1,16 +1,12 @@
 package algorithms.fundamentals.divideconquer;
 
-import org.junit.Test;
-
 /**
  * Created by Chen Li on 2018/6/28.
  */
-public class RecurrenceMaxSubarrayTest {
+public class RecurrenceMaxSubarrayTest extends AbstractMaxSubarrayTest {
 
-  @Test
-  public void name() {
-    RecurrenceMaxSubarray maxSubarray = new RecurrenceMaxSubarray();
-    Integer[] array =new Integer[]{13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
-    System.out.println(maxSubarray.find(array));
+  @Override
+  protected MaxSubarray getMaxSubarray() {
+    return new RecurrenceMaxSubarray();
   }
 }
