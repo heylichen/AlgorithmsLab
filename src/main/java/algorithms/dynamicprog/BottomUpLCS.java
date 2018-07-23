a2c8b7db69066c43a65b21280d567a60514f441a
heylichen <heylichen@qq.com>
2018-07-23 21:44:34 +0800

    optimize BottomUpLCS to use less memory

--- src/main/java/algorithms/dynamicprog/BottomUpLCS.java
+++ src/main/java/algorithms/dynamicprog/BottomUpLCS.java
@@ -15,24 +15,24 @@ public class BottomUpLCS implements LongestCommonSubSequence {
     }
     int m = x.length();
     int n = y.length();
-    Table<Integer, Integer, String> lcsTable = HashBasedTable.create();
+    Table<Integer, Integer, Integer> lcsTable = HashBasedTable.create();
     for (int i = 0; i < m; i++) {
-      lcsTable.put(i, -1, "");
+      lcsTable.put(i, -1, 0);
     }
     for (int j = 0; j < m; j++) {
-      lcsTable.put(-1, j, "");
+      lcsTable.put(-1, j, 0);
     }
-    lcsTable.put(-1, -1, "");
+    lcsTable.put(-1, -1, 0);
 
     for (int i = 0; i < m; i++) {
       for (int j = 0; j < n; j++) {
         if (x.charAt(i) == y.charAt(j)) {
-          String lcs = lcsTable.get(i - 1, j - 1) + x.charAt(i);
+          Integer lcs = lcsTable.get(i - 1, j - 1) + 1;
           lcsTable.put(i, j, lcs);
         } else {
-          String left = lcsTable.get(i - 1, j);
-          String right = lcsTable.get(i, j - 1);
-          if (left.length() > right.length()) {
+          Integer left = lcsTable.get(i - 1, j);
+          Integer right = lcsTable.get(i, j - 1);
+          if (left > right) {
             lcsTable.put(i, j, left);
           } else {
             lcsTable.put(i, j, right);
@@ -40,6 +40,21 @@ public class BottomUpLCS implements LongestCommonSubSequence {
         }
       }
     }
-    return lcsTable.get(m - 1, n - 1);
+    return viewLcs(lcsTable, x, y, m - 1, n - 1);
+  }
+
+  private String viewLcs(Table<Integer, Integer, Integer> lcsTable, String x, String y, int i, int j) {
+    if (i == -1 || j == -1) {
+      return "";
+    }
+    Integer maxLen = lcsTable.get(i, j);
+    Integer two = lcsTable.get(i - 1, j);
+    if (x.charAt(i) == y.charAt(j)) {
+      return viewLcs(lcsTable, x, y, i - 1, j - 1) + x.charAt(i);
+    } else if (maxLen.equals(two)) {
+      return viewLcs(lcsTable, x, y, i - 1, j);
+    } else {
+      return viewLcs(lcsTable, x, y, i, j - 1);
+    }
   }
 }
